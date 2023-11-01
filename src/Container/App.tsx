5ef3ee4f261e26a3647eb6b8a42268d7948d35eb
tankw <tantk113@gmail.com>
2023-11-01 23:32:21 +0700

    get cart data from app and add to slice state

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Footer, Header } from '../Components/Layout';
 import { Home, MenuItemDetails, NotFound } from '../Pages';
 import { Routes, Route } from 'react-router-dom';
+import { useGetCartQuery } from '../api/shoppingCartApi';
+import { useDispatch } from 'react-redux';
+import { setCart } from '../Storage/Redux/shoppingCartSlice';
 
 function App() {
+  const { data, isLoading, isSuccess, isError, error } = useGetCartQuery(
+    'ac131858-7e3c-47c6-8627-24bf078cb8b6'
+  );
+  const disPatch = useDispatch();
+  useEffect(() => {
+    if (isSuccess) {
+      disPatch(setCart(data.result?.cartItems));
+      //console.log(data);
+
+      console.log(data.result.cartItems);
+    }
+  }, [data]);
+
   return (
     <div className=" ">
       <Header></Header>
