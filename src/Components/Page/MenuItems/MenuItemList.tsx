1ecfe9c275ce4808f21bb31b79414e82dd808b18
tankw <tantk113@gmail.com>
2023-11-06 02:02:53 +0700

    category filter

--- src/Components/Page/MenuItems/MenuItemList.tsx
+++ src/Components/Page/MenuItems/MenuItemList.tsx
@@ -9,6 +9,9 @@ import { RootState } from '../../../Storage/Redux/store';
 var unorm = require('unorm');
 function MenuItemList() {
   const [menuItem, setMenuItems] = useState<menuItemModel[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [categoryList, setCategoryList] = useState(['']);
+
   const { data, isLoading, isSuccess, isError, error } =
     useGetMenuItemsQuery(null);
   const dispatch = useDispatch();
@@ -19,22 +22,35 @@ function MenuItemList() {
 
   useEffect(() => {
     if (data && data.result) {
-      const tempArray: menuItemModel[] = searchFilter(searchValue);
+      const tempArray: menuItemModel[] = searchFilter(
+        selectedCategory,
+        searchValue
+      );
+
       setMenuItems(tempArray);
     }
   }, [searchValue]);
 
-  const searchFilter = (search: string) => {
-    let tempMenuItems = [...data.result];
+  const searchFilter = (category: string, search: string) => {
+    let tempArray =
+      category === 'All'
+        ? [...data.result]
+        : data.result.filter(
+            (item: menuItemModel) =>
+              item.category.toLocaleUpperCase() === category.toLocaleUpperCase()
+          );
+
+    //let tempMenuItems = [...data.result];
+
     if (search) {
-      const temp = [...tempMenuItems];
-      tempMenuItems = temp.filter((item: menuItemModel) =>
+      const temp = [...tempArray];
+      tempArray = temp.filter((item: menuItemModel) =>
         removeVietnameseDiacritics(item.name)
           .toUpperCase()
           .includes(removeVietnameseDiacritics(search).toUpperCase())
       );
     }
-    return tempMenuItems;
+    return tempArray;
   };
   // const removeVietnameseDiacritics = (str: string) => {
   //   return str
@@ -53,8 +69,37 @@ function MenuItemList() {
     if (isSuccess) {
       dispatch(setMenuItem(data.result));
       setMenuItems(data.result);
+      //categories
+      const tempCategoryList = ['All'];
+      data.result.forEach((item: menuItemModel) => {
+        if (tempCategoryList.indexOf(item.category) === -1) {
+          tempCategoryList.push(item.category);
+        }
+      });
+      setCategoryList(tempCategoryList);
     }
   }, [isLoading]);
+
+  const handleCategory = (index: number) => {
+    const buttons = document.querySelectorAll('.custom-buttons');
+    let localCategory;
+    buttons.forEach((button, buttonIndex) => {
+      if (buttonIndex === index) {
+        button.classList.add('active');
+
+        if (index === 0) {
+          localCategory = 'All';
+        } else {
+          localCategory = categoryList[buttonIndex];
+        }
+        setSelectedCategory(localCategory);
+        const tempArray = searchFilter(localCategory, searchValue);
+        setMenuItems(tempArray);
+      } else {
+        button.classList.remove('active');
+      }
+    });
+  };
 
   return (
     <div>
@@ -65,7 +110,23 @@ function MenuItemList() {
       ) : !isSuccess ? (
         <div>Error while fetching data...Check console for error details</div>
       ) : (
-        <div className="row">
+        <div className="row ">
+          <div className="my-3 ">
+            <ul className="nav w-100 d-flex justify-content-center ">
+              {categoryList.map((categoryName, index) => (
+                <li className="nav-item" key={index}>
+                  <button
+                    className={`nav-link p-0 pb-2 custom-buttons fs-5  ${
+                      index === 0 && 'active'
+                    } `}
+                    onClick={() => handleCategory(index)}
+                  >
+                    <span className="text-white"> {categoryName}</span>
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
           {data.result.length > 0 &&
             menuItem.map((menuItem: menuItemModel, index: number) => {
               return <MenuItemCard key={index} menuItem={menuItem} />;
